7acf3a8b07fa35913c68bb464af1de0eb6390e6b
a-vargasmarte <a.vargasmarte@gmail.com>
2020-04-08 11:38:14 -0400

    added user baseline form onclick

--- public/js/baseline.js
+++ public/js/baseline.js
@@ -1,10 +1,20 @@
 $(document).ready(() => {
+  // when user clicks...
   $("#baseline-btn-acepto").on("click", () => {
+    // A larger form is appended to the DOM
+    appendUserForm();
+  });
+
+  const appendUserForm = () => {
     $("#baseline-content").empty();
     $("#terminos").empty();
-    let screenFieldColumns = [
-      ["Edad", "Parentezco", "Genero"],
-      ["Numero de ninos", "Comuna", "Edades de ninos"]
+    let screenFields = [
+      "Edad",
+      "Parentezco",
+      "Genero",
+      "Numero de ninos",
+      "Comuna",
+      "Edades de ninos"
     ];
     let efficacies = [
       "Siempre puedo resolver problemas si trato lo suficiente",
@@ -15,32 +25,57 @@ $(document).ready(() => {
     ];
 
     let inlineForm = $("<form>", {
-      id: "baseline-form",
-      class: "form-inline"
+      id: "baseline-form"
     });
     $("#baseline-content").append(inlineForm);
 
-    screenFieldColumns.map((columns, i) => {
-      columns.map((field, j) => {
-        let formGroupRow = $(`<div>`, { class: "form-group row>" });
-        let fieldLabel = $(`<label>`, {
-          for: `input${i}-${j}`,
-          class: "col-md-8 col-form-label"
-        }).text(field);
-        let formInputDiv = $(`<div>`, { class: "col-md-4" });
-        let formInput = $(`<input>`, {
-          type: "text",
-          //   class: "form-control",
-          id: `input${i}-${j}`
-        });
+    let formGroupRow = $(`<div>`, { class: "form-group row" });
+    inlineForm.append(formGroupRow);
 
-        formInputDiv.append(formInput);
-        formGroupRow.append(fieldLabel);
-        formGroupRow.append(formInputDiv);
-        console.log(formGroupRow);
+    screenFields.map((field, i) => {
+      let fieldLabel = $(`<label>`, {
+        for: `input${i}`,
+        class: "col-sm-3 col-form-label"
+      }).text(field);
+      let formInputDiv = $(`<div>`, { class: "col-sm-3" });
+      let formInput = $(`<input>`, {
+        type: "text",
+        class: "form-control",
+        id: `input${i}`
+      }).css("margin-bottom", "10px");
 
-        $("#baseline-form").append(formGroupRow);
-      });
+      formInputDiv.append(formInput);
+      formGroupRow.append(fieldLabel);
+      formGroupRow.append(formInputDiv);
+      // console.log(formGroupRow);
     });
-  });
+    let efficaciesRow = $("<div>", {
+      class: "form-group row",
+      id: "form-efficacies"
+    });
+
+    inlineForm.append(efficaciesRow);
+
+    efficacies.map((efficacy, i) => {
+      let efficacyLabel = $(`<label>`, {
+        for: `range${i}`,
+        class: "col-sm-6 col-form-label"
+      }).text(efficacy);
+
+      let formInputDiv = $(`<div>`, { class: "col-sm-6" });
+
+      let formInput = $(`<input>`, {
+        type: "range",
+        val: "0",
+        min: "0",
+        max: "100",
+        class: "form-control",
+        id: `range${i}`
+      }).css("margin-bottom", "10px");
+
+      formInputDiv.append(formInput);
+      efficaciesRow.append(efficacyLabel);
+      efficaciesRow.append(formInputDiv);
+    });
+  };
 });
