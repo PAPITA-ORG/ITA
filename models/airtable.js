b38aac8f531bc5ede1b869235a59560d6299efb0
a-vargasmarte <a.vargasmarte@gmail.com>
2020-04-16 21:16:42 -0400

    starter files for models and connection to mongodb atlas

--- models/airtable.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const AirTable = require("airtable");
-const baseID = process.env.API_BASE;
-const airKey = process.env.API_KEY;
-const base = new AirTable({ apiKey: airKey }).base(baseID);
-
-module.exports = base;
