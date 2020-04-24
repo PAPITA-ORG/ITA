40ecffaaaaa3610ef7785df66ede073d1c489c0f
juancaros <50739079+juancaros@users.noreply.github.com>
2020-04-24 09:30:47 -0400

    wip

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,27 @@
-// start express router
+// instantiate express router and store in variable `router`
 const router = require("express").Router();
 
-// import all routers
-const usuariosRouter = require("./usuarios");
+// import indexes controller
+const indexController = require("../../controllers").indexes;
 
-// route routers
-router.use("/usuarios", usuariosRouter);
+// match router with '/api/indexes'
 
-// export api router as route
+// get all indexes
+router.route("/").get(indexController.findAll);
+
+// get one index
+router.route("/:id").get(indexController.findByID);
+
+// delete all indexes
+router.route("/").delete(indexController.deleteMany);
+
+// delete one index
+router.route("/:id").delete(indexController.deleteOne);
+
+// update one index
+router.route("/:id").put(indexController.updateOne);
+
+// create one index
+router.route("/").post(indexController.create);
+
 module.exports = router;
