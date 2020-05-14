f6569e13b2c3d4e730ec47650ee915d5302b0bde
juancaros <50739079+juancaros@users.noreply.github.com>
2020-05-13 20:11:16 -0400

    wip

--- public/js/start.js
+++ public/js/start.js
@@ -9,6 +9,84 @@ $(document).ready(() => {
     body();
   });
 
+  $("#btn-diet").on("click", e => {
+    e.preventDefault();
+    diet();
+  });
+
+  $("#btn-mind").on("click", e => {
+    e.preventDefault();
+    mind();
+  });
+
+  let startContainer = $("#start-main-container");
+  
+  const mind = () => {
+    startContainer.empty();
+  
+    let mindRow = $("<div>", {
+      style: "margin-top: 20px",
+      class: "col-sm-12 form-group justify-center center",
+      id: "mind-row"
+    });
+  
+    let mindRowLabel = $("<div>", {
+      style: "margin-top: 20px",
+      class: "col-sm-12 form-group justify-center center",
+      id: "mind-row"
+    });
+
+    let potatoStatLabel = $("<label>", {
+      for: "start-potatostat"
+    }).text("No mind yet, here is a potato"); 
+
+    let potatoStat = $("<img>", {
+      src: "/images/papitacorp.jpg",
+      class: "center"
+    }).width(300).height(300);
+
+    mindRow.append(potatoStat);
+    mindRowLabel.append(potatoStatLabel);
+
+    startContainer.append(mindRowLabel);
+    startContainer.append(mindRow); 
+
+  };
+
+  let startContainer = $("#start-main-container");
+  
+  const diet = () => {
+    startContainer.empty();
+  
+    let dietRow = $("<div>", {
+      style: "margin-top: 20px",
+      class: "col-sm-12 form-group justify-center center",
+      id: "diet-row"
+    });
+  
+    let dietRowLabel = $("<div>", {
+      style: "margin-top: 20px",
+      class: "col-sm-12 form-group justify-center center",
+      id: "diet-row"
+    });
+
+    let potatoStatLabel = $("<label>", {
+      for: "start-potatostat"
+    }).text("No diet yet, here is a potato"); 
+
+    let potatoStat = $("<img>", {
+      src: "/images/papitacorp.jpg",
+      class: "center"
+    }).width(300).height(300);
+
+    dietRow.append(potatoStat);
+    dietRowLabel.append(potatoStatLabel);
+
+    startContainer.append(dietRowLabel);
+    startContainer.append(dietRow); 
+
+  };
+
   let startContainer = $("#start-main-container");
   
   const body = () => {
