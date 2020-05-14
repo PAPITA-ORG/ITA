3c2eb489835b9c675660aa36a2252337cf380e67
juancaros <50739079+juancaros@users.noreply.github.com>
2020-05-13 22:25:55 -0400

    start cycle

--- public/js/start.js
+++ public/js/start.js
@@ -6,17 +6,40 @@ $(document).ready(() => {
 
   $("#btn-body").on("click", e => {
     e.preventDefault();
-    body();
+    loading();
+    move();
+    setTimeout(function (){
+      selector();
+    }, 3000);
   });
 
   $("#btn-diet").on("click", e => {
     e.preventDefault();
-    diet();
+    loading();
+    move();
+    setTimeout(function (){
+      selector();
+    }, 3000); 
   });
 
   $("#btn-mind").on("click", e => {
     e.preventDefault();
-    mind();
+    loading();
+    move();
+    setTimeout(function (){
+      selector();
+    }, 3000);
+  });
+
+  $("#start-choice-btn").on("click", e => {
+    e.preventDefault();
+    //window.open("https://youtu.be/lGxNjqAjfkc", '_blank'); 
+    end();
+  });
+  
+  $("#start-form-btn").on("click", e => {
+    e.preventDefault();
+    window.location.href("/");
   });
 
   let startContainer = $("#start-main-container");
@@ -53,8 +76,6 @@ $(document).ready(() => {
 
   };
 
-  let startContainer = $("#start-main-container");
-  
   const diet = () => {
     startContainer.empty();
   
@@ -87,8 +108,6 @@ $(document).ready(() => {
 
   };
 
-  let startContainer = $("#start-main-container");
-  
   const body = () => {
     startContainer.empty();
   
@@ -152,5 +171,170 @@ $(document).ready(() => {
     startContainer.append(statsRow); 
 
   };
+
+  const selector = () => {
+    
+    startContainer.empty();
+    
+    let startSelector = $("<div>", {
+      style: "margin-top: 20px",
+      class: "col-sm-12 form-group justify-center center",
+      id: "start-selector"
+    });
+
+    let startIta = $("<img>", {
+      src: "/images/ita3d_1.png",
+      class: "center"
+    }).width(200).height(240);
+
+    let startSelectorLabel = $("<label>", {
+      for: "start-random-selector"
+    }).text("Quieres hacer esta actividad? (haz clic abajo)");
+
+    let startButton = $("<button>", {
+      type: "submit",
+      class: "btn btn-success",
+      id: "start-choice-btn"
+    }).text("Aprende actividades saludables con ITA");
+
+    linebreak = document.createElement("br");
+  
+    startSelector.append(startIta);  
+    startSelector.append(startSelectorLabel);
+    startSelector.append(linebreak);
+    startSelector.append(startButton);
+    startContainer.append(startSelector); 
+    
+  };  
+
+  const loading = () => {
+    
+    startContainer.empty();
+    
+    let startMessage = $("<div>", {
+      style: "margin-top: 20px",
+      class: "col-sm-12 form-group justify-center center",
+      id: "start-message"
+    });
+
+    let startIta = $("<img>", {
+      src: "/images/ita3d_1.png",
+      class: "center"
+    }).width(200).height(240);
+
+    let startRandomMessage = $("<label>", {
+      for: "start-random-message"
+    }).text("Practica, Practica, Practica");
+
+    let progressRow = $("<div>", {
+      //style: "margin-top: 20px",
+      //class: "col-sm-12 form-group justify-center center",
+      id: "myProgress"
+    });
+
+    let barRow = $("<div>", {
+      //style: "margin-top: 20px",
+      //class: "col-sm-12 form-group justify-center center",
+      id: "myBar"
+    });
+
+    startMessage.append(startIta);  
+    startMessage.append(startRandomMessage);
+    startContainer.append(startMessage); 
+    progressRow.append(barRow);
+    startContainer.append(progressRow); 
+  
+  };  
+
+  var i = 0;
+    function move() {
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById("myBar");
+      var width = 10;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + "%";
+          elem.innerHTML = width + "%";
+        }
+      }
+    }
+  }
+
+  function end() {
+    startContainer.empty();
+
+    // create a form
+    let startForm = $("<form>", {
+      id: "start-form"
+    });
+
+    // create a div with class form-group
+    let formGroup = $("<div>", {
+      class: "form-group"
+    });
+
+    // create a label
+    let emailLabel = $("<label>", {
+      for: "start-form-email"
+    }).text("Ingresa tus datos");
+
+     let passwordLabel = $("<label>", {
+      for: "start-form-password"
+    }).text("Contrasena"); 
+
+    // create email input tag
+    let emailInput = $("<input>", {
+      type: "email",
+      class: "form-control",
+      id: "start-form-email",
+      placeholder: "correo"
+    });
+
+    // create password input
+    let passwordInput = $("<input>", {
+      type: "password",
+      class: "form-control",
+      id: "start-form-password",
+      placeholder: "contraseña"
+    });
+
+    // create small tag
+    let emailSmall = $("<small>", {
+      class: "form-text text-muted"
+    }).text("We'll never share your verguita with anyone else");
+
+    // create small tag for password
+    let passwordSmall = $("<small>", {
+      class: "form-text text-muted"
+    }).text("Asegure que su contrasena sea de mas de 6 caracteres");
+
+    // create submit button
+
+    let submitButton = $("<button>", {
+      type: "submit",
+      class: "btn btn-success",
+      id: "start-form-btn"
+    }).text("Volver");
+
+    formGroup.append(emailLabel);
+    formGroup.append(emailInput);
+    //formGroup.append(emailSmall);
+
+    //formGroup.append(passwordLabel);
+    formGroup.append(passwordInput);
+    //formGroup.append(passwordSmall);
+     
+    startForm.append(formGroup);
+    startForm.append(submitButton);
+    
+    startContainer.append(startForm);
+
+  }; 
 
 });
