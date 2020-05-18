64f449f78b55053f0f913326f7d03a9bd4ea9cfb
juancaros <50739079+juancaros@users.noreply.github.com>
2020-05-18 15:31:46 -0400

    wip

--- models/comunas.js
+++ models/comunas.js
@@ -4,8 +4,12 @@ const Schema = mongoose.Schema;
 
 const Comunas = Schema({
   _id: { type: Schema.Types.ObjectId, required: true },
-  comuna: { type: Schema.Types.String, required: true },
-  comunaCod: { type: Schema.Types.Number, required: true }
+  regionCod: { type: Schema.Types.Number, required: true },
+  region: { type: Schema.Types.String, required: true },
+  provinciaCod: { type: Schema.Types.Number, required: true },
+  provincia: { type: Schema.Types.String, required: true },
+  comunaCod: { type: Schema.Types.Number, required: true },
+  comuna: { type: Schema.Types.String, required: true }  
 });
 
 const Comuna = mongoose.model("Comuna", Comunas);
