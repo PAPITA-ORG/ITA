87414f28752996ca4eecdd6c730c5ee10c94c496
Samuel Vargas <samuel.vargas.f@hotmail.com>
2020-05-20 15:49:39 -0400

    wip

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB_URI:
-    "mongodb+srv://juan-caro:H3xwwrNDjgSY8n4@ita-mwvea.mongodb.net/test?authSource=admin&replicaSet=ITA-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    "mongodb+srv://samuel:GMq8skb7kujEwhE@ita-mwvea.mongodb.net/test?authSource=admin&replicaSet=ITA-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true"
 };
