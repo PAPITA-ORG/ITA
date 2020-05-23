c9fee1c4ac31b7230e73ba790161e2a6dfe45616
Samuel Vargas <samuel.vargas.f@hotmail.com>
2020-05-22 21:38:11 -0400

    wip

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
-  var registro = 0
-  //condicion para tutorial o no
-  if(registro == 0){
-    //initialize swiper     
-    var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    $("#btn-finish").on("click", () => {
-    // condicion
-    });
-  }
-  
+  //initialize swiper     
+  var mySwiper = new Swiper ('.swiper-container', {
+
   });
+  $("#btn-finish").on("click", () => {
+    window.location.href = "start";
+  });
+  
+});
   
