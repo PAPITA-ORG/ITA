6f2c8ba46a60f64b4f8919c1c7b90f16c16ff501
juancaros <50739079+juancaros@users.noreply.github.com>
2020-05-25 11:12:38 -0400

    wip

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB_URI:
-    "mongodb+srv://juancaro:H3xwwrNDjgSY8n4@ita-mwvea.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://alberto-vargas:yyIOgmhHzQ6U8HzP@ita-mwvea.mongodb.net/test?retryWrites=true&w=majority"
 };
