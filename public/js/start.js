a5cb67cf391537f8ab21f0c3fe8400d99285e06f
juancaros <50739079+juancaros@users.noreply.github.com>
2020-06-04 08:35:47 -0400

    wip

--- public/js/start.js
+++ public/js/start.js
@@ -357,7 +357,7 @@ $(document).ready(() => {
 
         let rateChildLabel = $("<label>", {
           for: "rate-child-div"
-        }).text("Te gusto la actividad a tu niño(s) y/o niña(s)?");
+        }).text("Le gusto la actividad a tu niño(s) y/o niña(s)?");
 
  
         // ALBERTO ME VA A AYUDAR A HACER ESTO EN UNA FUNCION
