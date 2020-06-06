b2080453edfa5328f8ad495fe5b7a1b59e452772
juancaros <50739079+juancaros@users.noreply.github.com>
2020-06-06 15:42:03 -0400

    wip

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,78 @@
 $(document).ready(() => {
   let indexContainer = $("#index-container");
+
+  let pass;
+  $("input[type=password]").on("keyup", () => {
+    pass = $("#pw.form-control").val();
+    let repass = $("#pw2.form-control").val();
+    let largo = $("#pw2.form-control").val().length;
+
+    // condicion contraseñas iguales y espacio no en blanco
+    if (pass === repass && largo > 1) {
+      $("#btn-registro-enviar").removeAttr("disabled");
+    }
+  });
+
+  function passwordChange() {
+    indexContainer.empty();
+    
+    // create a form
+    let sobreForm = $("<form>", {
+      id: "sobre-form"
+    });
+
+    // create a div with class form-group
+    let formGroup = $("<div>", {
+      class: "form-group"
+    });
+
+    // create a label
+    let emailLabel = $("<label>", {
+      for: "sobre-form-email"
+    }).text("Ingresa tus datos");
+
+     let passwordLabel = $("<label>", {
+      for: "sobre-form-password"
+    }).text("Contrasena"); 
+
+    // create email input tag
+    let emailInput = $("<input>", {
+      type: "email",
+      class: "form-control",
+      id: "sobre-form-email",
+      placeholder: "correo"
+    });
+
+    // create password input
+    let passwordInput = $("<input>", {
+      type: "password",
+      class: "form-control",
+      id: "sobre-form-password",
+      placeholder: "contraseña"
+    });
+  }
+
+  // $("#btn-registro-enviar").on("click", e => {
+  //   let requestBody = {
+  //     correo: $("#email").val(),
+  //     password: pass
+  //   };
+
+  //   axios
+  //     .post("/api/usuarios/changePassword", requestBody)
+  //     .then(res => {
+  //       console.log(res);
+  //       // window.location.href = "/";
+  //     })
+  //     .catch(err => err);
+  // });
+
   $("#btn-si").on("click", () => {
     window.location.href = "sobre";
   });
 
   $("#btn-password").on("click", () => {
-    window.location.href = "registro";
+    passwordChange();
   });
 
   $("#btn-team").on("click", () => {
