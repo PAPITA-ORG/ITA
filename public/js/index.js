50834849318ce2fc3af4bdeb6c8471d319219193
juancaros <50739079+juancaros@users.noreply.github.com>
2020-06-09 08:26:56 -0400

    wip

--- public/js/index.js
+++ public/js/index.js
@@ -1,102 +1,15 @@
 $(document).ready(() => {
-  let indexContainer = $("#index-container");
 
-  function passwordChange() {
-      
-    indexContainer.empty();
-    
-    // create a form
-    let indexForm = $("<form>", {
-      id: "index-form"
-    });
+  let pass;
+  $("input[type=password]").on("keyup", () => {
+    pass = $("#pw.form-control").val();
+    let repass = $("#pw2.form-control").val();
+    let largo = $("#pw2.form-control").val().length;
 
-    // create a div with class form-group
-    let formGroup = $("<div>", {
-      class: "form-group"
-    });
-
-    // create a label
-    let emailLabel = $("<label>", {
-      for: "index-form-email"
-    }).text("Ingresa tus datos");
-
-     let pwLabel = $("<label>", {
-      for: "index-form-password"
-    }).text("Nueva contrasena"); 
-
-    let pw2Label = $("<label>", {
-      for: "index-form-password"
-    }).text("Repite la contrasena"); 
-
-    // create email input tag
-    let emailInput = $("<input>", {
-      type: "email",
-      class: "form-control",
-      sytle: "margin-top: 10px",
-      id: "index-form-email",
-      placeholder: "correo"
-    });
-
-    // create password input
-    let pwInput = $("<input>", {
-      type: "password",
-      class: "form-control",
-      sytle: "margin-top: 10px",
-      id: "index-form-password",
-      placeholder: "contraseña"
-    });
-
-    let pw2Input = $("<input>", {
-      type: "password",
-      class: "form-control",
-      sytle: "margin-top: 10px",
-      id: "index-form-password2",
-      placeholder: "repite contraseña"
-    });
-
-    let pwButton = $("<button>", {
-      class: "btn btn-success",
-      sytle: "margin-top: 10px",
-      type: "submit",
-      id: "password-btn-enviar",
-      disabled: 'disabled'
-    }).text("Enviar");
-
-    formGroup.append(emailLabel);
-    formGroup.append(emailInput);
-    formGroup.append(pwInput);
-    formGroup.append(pw2Input);
-    formGroup.append(pwButton);
-    indexForm.append(formGroup);
-    indexContainer.append(indexForm);
-
-    let pass;
-    $("input[type=password]").on("keyup", () => {
-      pass = $("#index-form-password").val();
-      let repass = $("#index-form-password2").val();
-      let largo = $("#index-form-password2").val().length;
-  
-      // condicion contraseñas iguales y espacio no en blanco
-      if (pass === repass && largo > 1) {
-        $("#password-btn-enviar").removeAttr("disabled");
-      }
-    });
-
-  }
-
-  $("#password-btn-enviar").on("click", e => {
-     let requestBody = {
-       correo: emailInput.val(),
-       password: pass
-     };
-
-     axios
-        .post("/api/usuarios/changePassword", requestBody)
-        .then(res => {
-          console.log(res);
-          window.location.href = "/";
-        })
-        .catch(err => err);
+    // condicion contraseñas iguales y espacio no en blanco
+    if (pass === repass && largo > 1) {
+      $("#btn-registro-enviar").removeAttr("disabled");
+    }
   });
 
   $("#btn-si").on("click", () => {
