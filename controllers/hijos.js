e68b6d7f7ec64e6b7bcf89e3d7416bfad6146e09
a-vargasmarte <a.vargasmarte@gmail.com>
2020-06-29 19:13:50 -0400

    removed hashing and salting of nombre field for hijos schema

--- controllers/hijos.js
+++ controllers/hijos.js
@@ -44,14 +44,10 @@ module.exports = {
   },
   // create one hijo
   create: (req, res) => {
-    let encryptedChildren = req.body.data.map(child => {
-      let hash = bcrypt.hashSync(child.nombre, 10);
-      child.nombre = hash;
-      return child;
-    });
+    let { data } = req.body;
 
     db.hijos
-      .insertMany(encryptedChildren)
+      .insertMany(data)
       .then(hijos => {
         let ids = hijos.map(hijo => {
           return hijo._id;
