2186c5c8ed8f93aded21f921bb0985d36ef9ca73
a-vargasmarte <a.vargasmarte@gmail.com>
2020-09-23 22:47:42 -0400

    formulario de hijes estilado para todos dispositivos

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -68,15 +68,15 @@ $(document).ready(function() {
     ];
 
     // create a label
-    let formLabel = $("<label>", {
-      for: "childForm"
+    let formLabel = $("<h2>", {
+      class: "mb-4"
     }).text("Ingresa los datos de tu niña/o para ayudarte mejor");
 
     childForm.append(formLabel);
 
     // create a div with class form-group
     let formGroup = $("<div>", {
-      class: "form-group row col-md-12"
+      class: "input-form-group form-group row col-md-12"
     });
     childForm.append(formGroup);
 
@@ -98,7 +98,7 @@ $(document).ready(function() {
       pattern: `"[A-Za-z\\s]*"`,
       title: "Solo use letras alfabeticas",
       id: "tutorial-form-nombre"
-    }).css("margin-bottom", "10px");
+    });
 
     let edadLabel = $("<label>", {
       for: "tutorial-form-edad",
@@ -109,7 +109,7 @@ $(document).ready(function() {
       class: "form-control",
       id: "tutorial-form-edad",
       type: "date"
-    }).css("margin-bottom", "10px");
+    });
 
     let generoLabel = $("<label>", {
       for: "tutorial-form-genero",
@@ -120,7 +120,7 @@ $(document).ready(function() {
       type: "text",
       class: "form-control",
       id: "tutorial-form-genero"
-    }).css("margin-bottom", "10px");
+    });
 
     generoList.map(list => {
       let option = $("<option>").text(list.nombre);
@@ -130,14 +130,14 @@ $(document).ready(function() {
     let tallaLabel = $("<label>", {
       for: "tutorial-form-talla",
       class: "col-sm-3 col-form-label"
-    }).text("Talla (mts)");
+    }).text("Talla (cm)");
 
     let tallaInput = $("<input>", {
       type: "number",
-      step: "0.01",
+      step: "1",
       class: "form-control",
       id: "tutorial-form-talla"
-    }).css("margin-bottom", "10px");
+    });
 
     let pesoLabel = $("<label>", {
       for: "tutorial-form-peso",
@@ -146,10 +146,10 @@ $(document).ready(function() {
 
     let pesoInput = $("<input>", {
       type: "number",
-      step: "0.1",
+      step: "1",
       class: "form-control",
       id: "tutorial-form-peso"
-    }).css("margin-bottom", "10px");
+    });
 
     let frecuenciaLabel = $("<label>", {
       for: "tutorial-form-frecuencia",
@@ -161,7 +161,7 @@ $(document).ready(function() {
       step: "1",
       class: "form-control",
       id: "tutorial-form-frecuencia"
-    }).css("margin-bottom", "10px");
+    });
 
     formGroupNombre.append(nombreInput);
     formGroupEdad.append(edadInput);
@@ -202,8 +202,8 @@ $(document).ready(function() {
       id: "form-emotions"
     });
 
-    let formInputLabel = $("<label>", {
-      for: "formInput"
+    let formInputLabel = $("<h2>", {
+      class: "mt-5 mb-5"
     }).text("Indique cuan de acuerdo esta con cada frase moviendo la barra");
 
     formGroup2.append(formInputLabel);
@@ -224,7 +224,7 @@ $(document).ready(function() {
         max: "100",
         class: "form-control efficacies-range mr-1",
         id: `range${i}`
-      }).css("margin-bottom", "10px");
+      });
 
       formInputDiv.append(formInput);
       emotionsRow.append(emotionsLabel);
@@ -234,7 +234,7 @@ $(document).ready(function() {
     let childFormDiv = $(`<div>`, { class: "text-center" });
 
     let childFormButton = $("<button>", {
-      class: "btn btn-success",
+      class: "btn btn-success tutorial-btn",
       id: "btn-child-form",
       style: "margin-right: 10px"
     }).text("Ir al menu");
@@ -242,7 +242,7 @@ $(document).ready(function() {
     childFormDiv.append(childFormButton);
 
     let childFormButton2 = $("<button>", {
-      class: "btn btn-success",
+      class: "btn btn-success tutorial-btn",
       id: "btn-child-form-2",
       style: "margin-left: 10px"
     }).text("Agregar otro");
