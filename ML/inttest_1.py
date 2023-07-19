0a85f8dfe6e6f58a49a50a664f50d693d722669b
juancaros <50739079+juancaros@users.noreply.github.com>
2023-07-19 16:06:23 -0400

    update

--- ML/inttest_1.py
+++ ML/inttest_1.py
@@ -6,12 +6,12 @@ import simpy
 import vowpalwabbit
 import random
 import matplotlib.pyplot as plt
-import statistics
-import time
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
 from joblib import Parallel, delayed
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
 
 # Baseline simulation: 1000 families in 20 villages and different expected treatment levels.
 
@@ -34,9 +34,9 @@ class users:
         self.gender = random.randint(0, 1)
         self.village = random.randint(1, 20)
         self.action = random.randint(1,4)
-        self.treat = random.normalvariate(0,0.2)*self.action
-        self.i0 = round(random.randint(0,10)*(1+(self.educ*0.15)+(self.mage*0.05))+self.gender*5)*self.treat 
-        self.s0 = round(random.randint(15,20)*(1+(self.educ*0.15)+(self.mage*0.05))+self.gender*5) + self.i0
+        self.treat = self.action*0.25 + self.action*0.25*(5-random.randint(1,4)+(self.educ*0.25)-(self.mage*0.1)+self.gender)
+        self.i0 = round(random.randint(1,5)*(1+(self.educ*0.15)+(self.mage*0.05))+self.gender*5) + round(random.randint(1,5)*(1+(self.educ*0.15)-(self.mage*0.05))+self.gender*5)*self.treat 
+        self.s0 = round(random.randint(1,5)*(1+(self.educ*0.15)-(self.mage*0.05))+self.gender*5) + round(random.randint(1,5)*(1+(self.educ*0.15)+(self.mage*0.05))+self.gender*5)*self.i0
         self.prob = 0.25 
 
 class Model:
@@ -72,146 +72,3 @@ if (__name__ == '__main__'):
     output_list_pure = list(output_list)
     output_df = pd.DataFrame(output_list_pure, columns=['user_id', 's0', 'i0', 'treat', 'action', 'village', 'educ', 'gender', 'mage', 'prob'])
 output_df.to_csv('inttest1.csv')             
-
-# Learning simulation (40 rounds each household with impact level given on baseline)
-
-df=pd.read_csv('inttest1.csv')
-# reward (impact) must enter as cost (negative)
-df['treat'] = -df['treat']
-dfcb = df
-
-from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error
-
-def train_xgboost_regressor(train_data, train_labels, test_data, test_labels):
-    # Define the XGBoost regressor
-    reg = XGBRegressor(
-        objective='reg:squarederror',
-        eval_metric='rmse',
-        max_depth=4,
-        learning_rate=0.18793869,
-        gamma=0.03805912,
-        min_child_weight=4.9259686,
-        subsample=0.89705926,
-        colsample_bytree=0.9011847,
-        seed=42
-    )
-    # Train the XGBoost model
-    reg.fit(train_data, train_labels)
-    # Evaluate the model on the test data
-    predictions = reg.predict(test_data)
-    mse = mean_squared_error(test_labels, predictions)
-    # Return the trained model and the mean squared error
-    return reg, mse
-
-#construct data for estimation
-train_df = df[df.index < 700]
-test_df = df[df.index >= 700]
-test_df.reset_index 
-X_train=train_df.drop(columns=['treat', 'prob', 'user_id'], axis=1, inplace=False)
-y_train=train_df['treat']
-X_test=test_df.drop(columns=['treat', 'prob', 'user_id'], axis=1, inplace=False)
-y_test=test_df['treat']
-# Train xgboost
-model, mse = train_xgboost_regressor(X_train, y_train, X_test, y_test)
-# Print the MSE
-print('MSE:', mse) 
-     
-def get_cost(s0, educ, gender, mage, i0, village, action, model):
-    
-    usuario=pd.DataFrame({'s0':s0, 'educ':educ, 'gender':gender, 'mage':mage, 'i0':i0, 'action':action, 'village':village}, index=[1])
-    costo=model.predict(usuario)
-    return costo[0]
-
-#Treatment menu (in-person short, in-person long, hybrid-short, hybrid-long)
-actions=[1,2,3,4]     
-
-#Update sample probabilities
-def sample_custom_pmf(pmf):
-    total = sum(pmf)
-    scale = 1 / total
-    pmf = [x * scale for x in pmf]
-    draw = random.random()
-    sum_prob = 0.0
-    for index, prob in enumerate(pmf):
-        sum_prob += prob
-        if sum_prob > draw:
-            return index, prob
-        
-# This function modifies (context, action, cost, probability) to VW friendly format
-def to_vw_example_format(context, actions, cb_label=None):
-    if cb_label is not None:
-        chosen_action, cost, prob = cb_label
-    example_string = ""
-    example_string += "shared |User user:{} mage:{} gender:{} i0:{} s0:{} educ:{} village{} \n".format(
-        context["user"], context["mage"], context["gender"], context["i0"], context["s0"], context["educ"], context["village"], 
-    )   
-    for action in actions:
-        if cb_label is not None and action == chosen_action:
-            example_string += "0:{}:{} ".format(cost, prob)
-        example_string += "|Action:{} \n".format(action)
-    # Strip the last newline
-    #print(example_string[:-1])
-    return example_string[:-1]
-
-# Suggest action based on learned reward value
-def get_action(vw, context, actions):
-    vw_text_example = to_vw_example_format(context, actions)
-    pmf = vw.predict(vw_text_example)
-    chosen_action_index, prob = sample_custom_pmf(pmf)
-    return actions[chosen_action_index], prob
-
-#Simulation setup for users over time
-def run_simulation(vw, dfcb, num_iterations, actions, get_action, get_cost, label):
-  
-    # das= Dificultad a Sugerir
-    das = []
-    act = []
-    
-    for j in range(len(dfcb)):
-        cost_sum = 0.0
-        cost_us = []
-        act_us = []
-        for i in range(1, num_iterations + 1):
-            
-            datos_vw=[]   
-            user = dfcb.iloc[j]['user_id']
-            gender=dfcb.iloc[j]['gender']
-            mage=dfcb.iloc[j]['mage']
-            s0=dfcb.iloc[j]['s0']
-            i0=dfcb.iloc[j]['i0']
-            educ=dfcb.iloc[j]['educ']
-            village=dfcb.iloc[j]['village']
-
-            contextos = {"user": user, "mage": mage, "gender":gender, "s0":s0, "i0":i0, "educ":educ, "village":village}
-            action, prob = get_action(vw, contextos, actions)
-            cost = get_cost(int(s0), int(educ), int(gender), int(float(mage)), int(i0), int(action), int(village), model)
-            cost_sum += float(cost)
-
-            if label:
-                vw_format=vw.parse(
-                    to_vw_example_format(contextos, actions, (action, cost, prob)),
-                    vowpalwabbit.LabelType.CONTEXTUAL_BANDIT)
-                vw.learn(vw_format)
-
-            else:
-                datos_vw.append(to_vw_example_format(contextos, actions))
-
-            cost_us.append(-1 * cost_sum / i)
-            act_us.append(action)
-        das.append(cost_us)
-        act.append(act_us)
-        
-        
-    return das, act   
-
-# Output plot
-def plot_das(num_iterations, das):
-    plt.plot(range(1, num_iterations + 1), das)
-    plt.xlabel("num_iterations", fontsize=14)
-    plt.ylabel("das", fontsize=14)
-
-vw = vowpalwabbit.Workspace("--cb_explore_adf -q UA --quiet --epsilon 0.2")
-num_iterations=40
-das, act = run_simulation(vw, dfcb, num_iterations, actions, get_action, get_cost, label=True)
-plot_das(num_iterations, das[40])        
